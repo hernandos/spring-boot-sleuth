42eeef7419321f8ca0b4745e294cfd4cece79641
Hernando Saenz <hernandos@gmail.com>
2022-12-05 16:43:38 +0100

    added test controller

---
+++ src/main/kotlin/de/com/saenz/springboot3withcloudsleuth/web/TestController.kt
@@ -0,0 +1,17 @@
+package de.com.saenz.springboot3withcloudsleuth.web
+
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+
+    val log = LoggerFactory.getLogger(TestController::class.java)
+    @GetMapping("/hello")
+    fun index(@RequestParam("name") name: String): String {
+        log.info("hello controller requested")
+        return "Hello, $name!"
+    }
+}
