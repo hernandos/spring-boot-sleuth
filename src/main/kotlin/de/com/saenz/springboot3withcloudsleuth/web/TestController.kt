3690e1884b4dec1d805e5ee7de2b2defd9f1d104
Hernando Saenz <hernandos@gmail.com>
2022-12-05 16:44:56 +0100

    added explizit type for logger in controller

--- src/main/kotlin/de/com/saenz/springboot3withcloudsleuth/web/TestController.kt
+++ src/main/kotlin/de/com/saenz/springboot3withcloudsleuth/web/TestController.kt
@@ -1,5 +1,6 @@
 package de.com.saenz.springboot3withcloudsleuth.web
 
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TestController {
 
-    val log = LoggerFactory.getLogger(TestController::class.java)
+    val log: Logger = LoggerFactory.getLogger(TestController::class.java)
     @GetMapping("/hello")
     fun index(@RequestParam("name") name: String): String {
         log.info("hello controller requested")
