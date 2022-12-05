f8d24f951ab10984ad9652fa4b3a1bbb9d0cc09d
Hernando Saenz <hernandos@gmail.com>
2022-12-05 15:11:14 +0100

    Initial commit - project created

---
+++ src/main/kotlin/de/com/saenz/springboot3withcloudsleuth/SpringBoot3WithCloudSleuthApplication.kt
@@ -0,0 +1,11 @@
+package de.com.saenz.springboot3withcloudsleuth
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringBoot3WithCloudSleuthApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringBoot3WithCloudSleuthApplication>(*args)
+}
---
+++ src/test/kotlin/de/com/saenz/springboot3withcloudsleuth/SpringBoot3WithCloudSleuthApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.com.saenz.springboot3withcloudsleuth
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBoot3WithCloudSleuthApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
